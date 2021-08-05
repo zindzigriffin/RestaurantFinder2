e7baa395f4b4ece009dea89f7d57d3312ea7e58b
Zindzi Mikal Kai Griffin <zindzigriffin@fb.com>
2021-08-05 01:15:09 -0700

    added some omments + cleaned up debug lines in search fragment tab

--- app/src/main/java/com/example/restaurantfinder2/fragments/SearchFragment.java
+++ app/src/main/java/com/example/restaurantfinder2/fragments/SearchFragment.java
@@ -35,6 +35,7 @@ import okhttp3.Headers;
 //The search fragment allows the user to search for dishes by entering some ingredients into a searchbar.
 //TODO: Cleanup debug lines (log statements and commented out code)
 public class SearchFragment extends Fragment {
+    //Declare variables
     public static final String APP_ID = "5420521";
     public static final String API_KEY = "960b664e1bmsh613e17b2b2ceab3p1a2fe3jsnce5dbacacf52";
     public static final String BASE_URL = "https://www.themealdb.com/api/json/v1/1/filter.php";
@@ -48,41 +49,40 @@ public class SearchFragment extends Fragment {
         // Required empty public constructor
     }
 
-
+    //Method to inflate the layout for this fragment and initialize all of the objects used in this fragment
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_search, container, false);
     }
-
+    //This is the method where any view lookups and attaching listeners are used
     @Override
     public void onViewCreated(@NonNull @NotNull View view, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         // Setting toolbar
         Toolbar toolbar = view.findViewById(R.id.toolbar);
         recyclerViewRecipes = view.findViewById(R.id.rvRecipes);
-        Log.i(TAG, "hi");
         // Initialize the adapter
         recipesAdapter = new RecipesAdapter(getContext(), aRecipes);
         // Attach the adapter to the RecyclerView
         recyclerViewRecipes.setAdapter(recipesAdapter);
-        Log.i(TAG, "hi1");
         // Set layout manager to position the items
         recyclerViewRecipes.setLayoutManager(new LinearLayoutManager(getContext()));
-        Log.i(TAG, "hi2");
 
         //Get data from the searchView & query the API to get the results (Recipes)
         final SearchView searchView = view.findViewById(R.id.search_view);
+        //Sets a listener for user actions within the SearchView.
+        //listener – the listener object that receives callbacks when the user performs actions in the SearchView such as clicking on buttons or typing a query.
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            //Called when the user submits the query.
             @Override
             public boolean onQueryTextSubmit(String query) {
-                Log.d(TAG, "success!");
                 fetchRecipes(query);
                 Toast.makeText(getContext(), "Our word : " + query, Toast.LENGTH_SHORT).show();
                 return true;
             }
-
+            //Called when the query text is changed by the user.
             @Override
             public boolean onQueryTextChange(String newText) {
                 return false;
@@ -94,7 +94,7 @@ public class SearchFragment extends Fragment {
     public void fetchRecipes(String query) {
         //creating an instance of AsyncHttpClient
         AsyncHttpClient client = new AsyncHttpClient();
-        Log.i(TAG, "query" + query);
+        //creating an instance of RequestParams
         RequestParams params = new RequestParams();
         params.put("i",query);
         RequestHeaders header = new RequestHeaders();
@@ -102,22 +102,18 @@ public class SearchFragment extends Fragment {
         header.put("x-rapidapi-key", "960b664e1bmsh613e17b2b2ceab3p1a2fe3jsnce5dbacacf52");
         header.put("x-rapidapi-host", "themealdb.p.rapidapi.com");
         //making a get request to the API
-        Log.d(TAG, "onSuccess");
         client.get(BASE_URL, params, new JsonHttpResponseHandler() {
             @Override
             public void onSuccess(int statusCode, Headers headers, JSON json) {
-                //printing onSuccess if response is successful
-                Log.d(TAG, "onSuccess2");
+                //set the json object
                 JSONObject jsonObject = json.jsonObject;
-                Log.i(TAG, "recipes2");
                 try {
                     //Returns the value mapped by name if it exists and is a JSONArray, or throws otherwise.
                     JSONArray meals = jsonObject.getJSONArray("meals");
                     List<Recipes> recipes = Recipes.fromJSONArray(meals);
-                    Log.i(TAG, "recipes");
+                    //add all the elements to the list
                     aRecipes.addAll(recipes);
-                    recipesAdapter.notifyDataSetChanged();
-                    Log.i(TAG, "Meals" + meals.toString());
+                    //notify the adapter that the data has changed
                     recipesAdapter.notifyDataSetChanged();
                 } catch (JSONException e) {
                     Log.e(TAG, "hit JSON Exception", e);
